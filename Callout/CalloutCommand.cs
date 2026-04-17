65911e7adceaf20d6ca11e5ebc3d7b3791fdda87
Hoàng Tâm <hvtam9archi@gmail.com>
2026-04-17 15:22:03 +0700

    Add project files.

---
+++ Callout/CalloutCommand.cs
@@ -0,0 +1,140 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.DatabaseServices.Filters;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.Runtime;
+using Callout.Jigs;
+using Callout.Services;
+
+[assembly: CommandClass(typeof(Callout.Commands.CalloutCommand))]
+
+namespace Callout.Commands
+{
+	public class CalloutCommand
+	{
+		[CommandMethod("CT")]
+		public void CreateCallout()
+		{
+			Document doc = Application.DocumentManager.MdiActiveDocument;
+			Database db = doc.Database;
+			Editor ed = doc.Editor;
+
+			using (DocumentLock docLock = doc.LockDocument())
+			using (Transaction tr = db.TransactionManager.StartTransaction())
+			{
+				try
+				{
+					// 1. Nhập liệu
+					var optBlk = new PromptEntityOptions("\nChọn Block cần trích: ");
+					optBlk.SetRejectMessage("\nChỉ chọn BlockReference!");
+					optBlk.AddAllowedClass(typeof(BlockReference), true);
+					var resBlk = ed.GetEntity(optBlk);
+					if (resBlk.Status != PromptStatus.OK) return;
+
+					var optPl = new PromptEntityOptions("\nChọn Polyline làm khung: ");
+					optPl.SetRejectMessage("\nChỉ chọn Polyline!");
+					optPl.AddAllowedClass(typeof(Polyline), true);
+					var resPl = ed.GetEntity(optPl);
+					if (resPl.Status != PromptStatus.OK) return;
+
+					var sourceBlock = tr.GetObject(resBlk.ObjectId, OpenMode.ForRead) as BlockReference;
+					var boundaryPoly = tr.GetObject(resPl.ObjectId, OpenMode.ForRead) as Polyline;
+
+					Extents3d origExt = boundaryPoly.GeometricExtents;
+					Point3d basePoint = new Point3d(
+						(origExt.MinPoint.X + origExt.MaxPoint.X) / 2.0,
+						(origExt.MinPoint.Y + origExt.MaxPoint.Y) / 2.0,
+						(origExt.MinPoint.Z + origExt.MaxPoint.Z) / 2.0
+					);
+
+					// 2. Kỹ thuật Đỉnh cao: Gói chi tiết vào một Anonymous Block (*U)
+					BlockTable bt = (BlockTable)tr.GetObject(db.BlockTableId, OpenMode.ForWrite);
+					BlockTableRecord btrAnon = new BlockTableRecord
+					{
+						Name = "*U" // Báo cho AutoCAD đây là Block ẩn
+					};
+					ObjectId anonId = bt.Add(btrAnon);
+					tr.AddNewlyCreatedDBObject(btrAnon, true);
+
+					// Clone và dời gốc tọa độ về tâm của block ẩn
+					BlockReference innerBlock = sourceBlock.Clone() as BlockReference;
+					Polyline innerPoly = boundaryPoly.Clone() as Polyline;
+					Vector3d toOrigin = basePoint.GetVectorTo(Point3d.Origin);
+
+					innerBlock.TransformBy(Matrix3d.Displacement(toOrigin));
+					innerPoly.TransformBy(Matrix3d.Displacement(toOrigin));
+
+					btrAnon.AppendEntity(innerBlock);
+					tr.AddNewlyCreatedDBObject(innerBlock, true);
+					btrAnon.AppendEntity(innerPoly);
+					tr.AddNewlyCreatedDBObject(innerPoly, true);
+
+					// Áp dụng XClip vào thành phần bên trong (XClip sẽ hoạt động hoàn hảo)
+					ApplyXClip(tr, innerBlock, innerPoly);
+
+					// Tạo đối tượng tham chiếu kéo thả trên RAM
+					BlockReference jigRef = new BlockReference(basePoint, anonId);
+
+					// 3. Chạy Jig
+					JigInputHandler.Start();
+					var calloutJig = new CalloutJig(jigRef, basePoint, origExt);
+					PromptResult jigRes = ed.Drag(calloutJig);
+					JigInputHandler.Stop();
+
+					// 4. Kết thúc lệnh
+					if (jigRes.Status == PromptStatus.OK)
+					{
+						BlockTableRecord cSpace = (BlockTableRecord)tr.GetObject(db.CurrentSpaceId, OpenMode.ForWrite);
+
+						// Thêm Block trích xuất
+						jigRef.Position = calloutJig.CurrentPosition;
+						jigRef.ScaleFactors = new Scale3d(JigInputHandler.CurrentScale);
+						cSpace.AppendEntity(jigRef);
+						tr.AddNewlyCreatedDBObject(jigRef, true);
+
+						// Thêm đường Leader nối
+						Polyline finalLeader = CalloutGeometryService.CreateSmartLeader(origExt, calloutJig.MathTransform);
+						cSpace.AppendEntity(finalLeader);
+						tr.AddNewlyCreatedDBObject(finalLeader, true);
+
+						tr.Commit();
+					}
+					else
+					{
+						tr.Abort();
+					}
+				}
+				catch (System.Exception ex)
+				{
+					ed.WriteMessage($"\nLỗi: {ex.Message}");
+					tr.Abort();
+				}
+			}
+		}
+
+		private void ApplyXClip(Transaction tr, BlockReference br, Polyline pl)
+		{
+			br.CreateExtensionDictionary();
+			DBDictionary extDict = (DBDictionary)tr.GetObject(br.ExtensionDictionary, OpenMode.ForWrite);
+
+			DBDictionary filterDict = new DBDictionary();
+			extDict.SetAt("ACAD_FILTER", filterDict);
+			tr.AddNewlyCreatedDBObject(filterDict, true);
+
+			Matrix3d wcsToBlk = br.BlockTransform.Inverse();
+			Point2dCollection pts = new Point2dCollection();
+			for (int i = 0; i < pl.NumberOfVertices; i++)
+			{
+				Point3d ptBlk = pl.GetPoint3dAt(i).TransformBy(wcsToBlk);
+				pts.Add(new Point2d(ptBlk.X, ptBlk.Y));
+			}
+
+			SpatialFilterDefinition sfd = new SpatialFilterDefinition(pts, Vector3d.ZAxis, 0.0, 0.0, 0.0, true);
+			SpatialFilter sf = new SpatialFilter() { Definition = sfd };
+
+			filterDict.SetAt("SPATIAL", sf);
+			tr.AddNewlyCreatedDBObject(sf, true);
+		}
+	}
+}
---
+++ Callout/CalloutJig.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+using Callout.Services;
+using AcadApp = Autodesk.AutoCAD.ApplicationServices.Application;
+
+namespace Callout.Jigs
+{
+	public class CalloutJig : DrawJig
+	{
+		public BlockReference JigRef { get; private set; }
+		public Point3d CurrentPosition { get; private set; }
+		public Matrix3d MathTransform { get; private set; }
+
+		private readonly Point3d _basePoint;
+		private readonly Extents3d _origExtents;
+
+		public CalloutJig(BlockReference jigRef, Point3d basePoint, Extents3d origExt)
+		{
+			JigRef = jigRef;
+			_basePoint = basePoint;
+			_origExtents = origExt;
+			CurrentPosition = basePoint;
+		}
+
+		protected override SamplerStatus Sampler(JigPrompts prompts)
+		{
+			JigPromptPointOptions opt = new JigPromptPointOptions("\n+/- Tỷ lệ): ")
+			{
+				UserInputControls = UserInputControls.Accept3dCoordinates | UserInputControls.NullResponseAccepted | UserInputControls.GovernedByOrthoMode,
+				UseBasePoint = true,
+				BasePoint = _basePoint
+			};
+
+			PromptPointResult res = prompts.AcquirePoint(opt);
+			if (res.Status == PromptStatus.Cancel) return SamplerStatus.Cancel;
+
+			if (JigInputHandler.ScaleChanged || res.Value.DistanceTo(CurrentPosition) > 0.001)
+			{
+				CurrentPosition = res.Value;
+				JigInputHandler.ScaleChanged = false;
+				return SamplerStatus.OK;
+			}
+
+			return SamplerStatus.NoChange;
+		}
+
+		protected override bool WorldDraw(Autodesk.AutoCAD.GraphicsInterface.WorldDraw draw)
+		{
+			JigRef.Position = CurrentPosition;
+			JigRef.ScaleFactors = new Scale3d(JigInputHandler.CurrentScale);
+
+			// Vẽ khối trích xuất
+			draw.Geometry.Draw(JigRef);
+
+			// Tính ma trận thuần túy để dóng đường Leader
+			MathTransform = Matrix3d.Scaling(JigInputHandler.CurrentScale, CurrentPosition) * Matrix3d.Displacement(_basePoint.GetVectorTo(CurrentPosition));
+
+			// Vẽ đường nối
+			using (Polyline leader = CalloutGeometryService.CreateSmartLeader(_origExtents, MathTransform))
+			{
+				draw.Geometry.Draw(leader);
+			}
+
+			// --- TÍNH NĂNG MỚI: HEADS-UP DISPLAY (Hiện Tỷ Lệ cạnh con trỏ) ---
+			double viewSize = (double)AcadApp.GetSystemVariable("VIEWSIZE");
+			using (DBText txt = new DBText())
+			{
+				// Căn lùi vị trí Text xéo xuống dưới một chút cho khỏi đè vào chuột
+				txt.Position = CurrentPosition + new Vector3d(viewSize * 0.03, -viewSize * 0.03, 0);
+				txt.Height = viewSize * 0.02; // Tự động to nhỏ phù hợp với màn hình
+				txt.TextString = $"Ty le: {JigInputHandler.CurrentScale}x";
+				txt.ColorIndex = 2; // Màu vàng
+				draw.Geometry.Draw(txt);
+			}
+
+			return true;
+		}
+	}
+
+	/// <summary>
+	/// Native Hook chặn phím, đổi Tỷ Lệ và xuất Text ra Command Line
+	/// </summary>
+	public static class JigInputHandler
+	{
+		public static double CurrentScale = 1.0;
+		public static bool ScaleChanged = false;
+
+		[DllImport("user32.dll")]
+		private static extern bool SetCursorPos(int X, int Y);
+
+		[DllImport("user32.dll")]
+		[return: MarshalAs(UnmanagedType.Bool)]
+		private static extern bool GetCursorPos(out POINT lpPoint);
+
+		[StructLayout(LayoutKind.Sequential)]
+		public struct POINT { public int X; public int Y; }
+
+		public static void Start()
+		{
+			CurrentScale = 1.0;
+			ScaleChanged = false;
+			AcadApp.PreTranslateMessage += OnPreTranslateMessage;
+		}
+
+		public static void Stop()
+		{
+			AcadApp.PreTranslateMessage -= OnPreTranslateMessage;
+		}
+
+		private static void OnPreTranslateMessage(object sender, Autodesk.AutoCAD.ApplicationServices.PreTranslateMessageEventArgs e)
+		{
+			if (e.Message.message == 0x0100) // WM_KEYDOWN
+			{
+				int vkCode = (int)e.Message.wParam;
+				bool handled = false;
+
+				if (vkCode == 38 || vkCode == 87 || vkCode == 107 || vkCode == 187)
+				{
+					CurrentScale += 1.0;
+					handled = true;
+				}
+				else if (vkCode == 40 || vkCode == 83 || vkCode == 109 || vkCode == 189)
+				{
+					if (CurrentScale > 1.0) CurrentScale -= 1.0;
+					handled = true;
+				}
+
+				if (handled)
+				{
+					ScaleChanged = true;
+					e.Handled = true; // Tiêu hủy phím, không dính vào Command Line
+
+					// Hiển thị ra Command Line
+					try
+					{
+						var doc = AcadApp.DocumentManager.MdiActiveDocument;
+						doc?.Editor.WriteMessage($"\n>> Tỷ lệ trích xuất hiện tại: {CurrentScale}x");
+					}
+					catch { }
+
+					// Ních chuột 1 pixel để cập nhật hình ngay lập tức
+					if (GetCursorPos(out POINT p)) SetCursorPos(p.X + 1, p.Y);
+				}
+			}
+		}
+	}
+}
